9264a3d616715ef39ac1f8f135c0c56495b56a4e
Baptiste Vanhuyse <baptiste.vanhuyse@gmail.com>
2023-05-12 15:48:56 +0200

    essai de l'ajout du filtre par category

--- articles/models.py
+++ articles/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from storages.backends.s3boto3 import S3Boto3Storage
 
 class Article(models.Model):
     nom = models.CharField(max_length=255)
     description = models.TextField()
     categorie = models.CharField(max_length=255)
     prix = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='articles_images/')
+    image = models.ImageField(storage=S3Boto3Storage(), upload_to='articles_images/')
     promotion = models.ForeignKey('promotion.Promotion', on_delete=models.SET_NULL, null=True, blank=True)
--- articles/views.py
+++ articles/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from .models import Article
 
 def liste_articles(request):
-    articles = Article.objects.all()
+    category = request.GET.get('category')
+    if category:
+        articles = Article.objects.filter(categorie=category)
+    else:
+        articles = Article.objects.all()
     for article in articles:
         if article.promotion:
             article.promo_price = article.prix - (article.prix * article.promotion.pourcentage / 100)
         else:
             article.promo_price = None
-    return render(request, 'articles/liste_articles.html', {'articles': articles})
+    return render(request, 'articles/article_list.html', {'articles': articles})
+
 
 
 def home(request):
