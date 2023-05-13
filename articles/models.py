a49e9dbe1524beb106ffa9adb48fd6416c88bee9
Baptiste Vanhuyse <baptiste.vanhuyse@gmail.com>
2023-05-13 08:43:05 +0200

    correction v6

--- articles/models.py
+++ articles/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from storages.backends.s3boto3 import S3Boto3Storage
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
 class Article(models.Model):
     nom = models.CharField(max_length=255)
     description = models.TextField()
-    categorie = models.CharField(max_length=255)
+    categorie = models.ForeignKey(Category, on_delete=models.CASCADE)
     prix = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(storage=S3Boto3Storage(), upload_to='articles_images/')
     promotion = models.ForeignKey('promotion.Promotion', on_delete=models.SET_NULL, null=True, blank=True)
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Article
+from .models import Article, Category
 
 def liste_articles(request):
     category = request.GET.get('categorie')
@@ -7,12 +7,14 @@ def liste_articles(request):
         articles = Article.objects.filter(categorie=category)
     else:
         articles = Article.objects.all()
+    categories = Category.objects.all()
     for article in articles:
         if article.promotion:
             article.promo_price = article.prix - (article.prix * article.promotion.pourcentage / 100)
         else:
             article.promo_price = None
-    return render(request, 'articles/liste_articles.html', {'articles': articles})
+    return render(request, 'articles/liste_articles.html', {'articles': articles, 'categories': categories})  # Modification de cette ligne
+
 
 def home(request):
     articles = Article.objects.all()
